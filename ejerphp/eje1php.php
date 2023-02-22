a4897e77e45d76b424fff57834f68520283b99b8
Jhanfrancodiaz <jhanfrancoo123@gmail.com>
2023-02-21 22:54:53 -0500

    correcciones

--- ejerphp/eje1php.php
+++
@@ -1,8 +0,0 @@
-<?php
-$alturac= $_GET["alturac"];
-echo "LA ALTURA INGRESADA ES:".$alturac;
-$rta= $alturac/2.54;
-$rta2= $rta/12;
- echo"LA ALTURA INGRESADA EN PULGADAS ES:" . $rta ;
-echo "LA ALTURA INGRESADA EN PIES ES:" .$rta2;
-?>
